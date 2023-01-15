77e2dc5d90d0a801af1a0bb0ac33c4769c4a9178
Erickuby <ericcnwankwo@gmail.com>
2023-01-15 00:01:51 +0000

    Start quiz, question and answer button created

--- assets/js/questions.js
+++ assets/js/questions.js
@@ -1,23 +1,5 @@
 const questions = [
     {
-      question: "What is the correct syntax for referring to an external script called 'xxx.js'?",
-      choices: [
-        "<script href='xxx.js'>",
-        "<script name='xxx.js'>",
-        "<script src='xxx.js'>"
-      ],
-      answer: 2
-    },
-    {
-      question: "Inside which HTML element do we put the JavaScript?",
-      choices: [
-        "<javascript>",
-        "<script>",
-        "<js>"
-      ],
-      answer: 1
-    },
-    {
       question: "What is the correct syntax for creating a function in JavaScript?",
       choices: [
         "function:myFunction()",
@@ -43,7 +25,26 @@ const questions = [
         "if i == 5 then"
       ],
       answer: 0
-    }
+    },
+    {
+      question: "What is the correct syntax for creating a variable in JavaScript?",
+      choices: [
+        "var carName",
+        "variable carName",
+        "let carName"
+      ],
+      answer: 0
+    },
+    {
+      question: "What is the correct syntax for an if statement in JavaScript?",
+      choices: [
+        "if (i == 5)",
+        "if i = 5 then",
+        "if i == 5 then"
+      ],
+      answer: 0
+    },
+    
     // Add more questions as needed
   ];
   
